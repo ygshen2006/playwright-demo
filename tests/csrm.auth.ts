0cef1601257729b1d76747ba1a3b653a0896a90f
ygshen200@163.com <ygshen200@163.com>
2024-04-10 21:39:54 +0800

    reset

--- tests/csrm.auth.ts
+++ tests/csrm.auth.ts
@@ -2,12 +2,12 @@ import { test as setup } from '@playwright/test'
 const authFile = 'playwright/.auth/csrm.json';
 setup('auth', async ({ page }, testInfo) => {
     testInfo.setTimeout(testInfo.timeout + 60000);
-    await page.goto('https://cncsrmsit.dyson.cn/#/login?redirect=%2Fdashboard');
+    await page.goto('https://xs/#/login?redirect=%2Fdashboard');
     await page.locator('#tab-0').click();
     await page.locator('#pane-0 [name="username"]').fill("jcshen");
-    await page.locator('#pane-0 [name="password"]').fill("User@123");
+    await page.locator('#pane-0 [name="password"]').fill("@123");
     await page.locator('#pane-0 .el-button--primary').click();
     
-    await page.waitForURL('https://cncsrmsit.dyson.cn/#/dashboard');
+    await page.waitForURL('https://xs/#/dashboard');
     await page.context().storageState({ path: authFile });
 });
